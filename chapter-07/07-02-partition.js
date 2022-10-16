6b4ab127cfd72f54cc47467cbb80fcf0e593b89a
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-10-16 12:22:05 +0900

    Added solution2 for chapter-07/07-02-partition.js

--- chapter-07/07-02-partition.js
+++ chapter-07/07-02-partition.js
@@ -1,6 +1,9 @@
 // 入力
 // const n = 12; // 配列Aの要素数
-const A = [13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11, 100, 23];
+// const A = [13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11];
+const A = Array.from({ length: 10000000 }).map((_, i, array) =>
+  Math.floor(Math.random() * array.length)
+);
 
 // 出力
 // [9, 5, 8, 7, 4, 2, 6, [11], 21, 13, 19, 12]
@@ -13,9 +16,7 @@ const A = [13, 19, 9, 5, 12, 8, 7, 4, 21, 2, 6, 11, 100, 23];
  *
  * @param {Array<number>} array
  */
-function solution(array = A) {
-  console.log({ before: array });
-
+function solution1(array) {
   const lastIndex = array.length - 1;
   const x = array[lastIndex];
   let i = -1;
@@ -38,6 +39,31 @@ function solution(array = A) {
     [array[i + 1], array[lastIndex]] = [[array[lastIndex]], array[i + 1]];
   }
 
-  console.log({ after: array });
+  // console.log({ after: array });
 }
-solution();
+console.time('solution1');
+solution1([...A]);
+console.timeEnd('solution1');
+
+/**
+ *
+ * @param {Array<number>} array
+ */
+function solution2(array) {
+  const copiedArray = [...array];
+  const lastValue = copiedArray.pop();
+  const smallGroup = [];
+  const bigGroup = [];
+
+  // copiedArrayはpopしているため、オリジナルのarrayより要素数が1少なくなっている
+  for (let i = 0; i < copiedArray.length; i++) {
+    const current = copiedArray[i];
+    current <= lastValue ? smallGroup.push(current) : bigGroup.push(current);
+  }
+  const partitionArray = [...smallGroup, [lastValue], ...bigGroup];
+
+  // console.log({ partitionArray });
+}
+console.time('solution2');
+solution2([...A]);
+console.timeEnd('solution2');
